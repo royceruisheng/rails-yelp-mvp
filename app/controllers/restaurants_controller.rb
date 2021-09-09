3d5549332a4f118042303c8dfd26faff74083de9
royceruisheng <ruisheng.lim@gmail.com>
2021-09-09 14:00:26 +0800

    update

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,33 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
+
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    if Restaurant.create(restaurant_params)
+      raise
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
